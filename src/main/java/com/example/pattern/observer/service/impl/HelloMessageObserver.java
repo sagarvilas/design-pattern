597a402377158eb8d4b1d514206010ce12459579
sagarvilas <sagar.vilas@gmail.com>
2021-06-06 13:10:33 +0200

    Update gitignore

--- src/main/java/com/example/pattern/observer/service/impl/HelloMessageObserver.java
+++ src/main/java/com/example/pattern/observer/service/impl/HelloMessageObserver.java
@@ -1,17 +1,19 @@
 package com.example.pattern.observer.service.impl;
 
 import com.example.pattern.observer.service.api.Observer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.beans.PropertyChangeEvent;
 
 @Service
+@Slf4j
 public class HelloMessageObserver implements Observer {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if("hello".equalsIgnoreCase((String) evt.getNewValue())){
-            System.out.println("Hello message was sent");
+            log.info("*****Hello message was sent*****");
         }
     }
 }
--- src/main/java/com/example/pattern/observer/service/impl/HiMessageObserver.java
+++ src/main/java/com/example/pattern/observer/service/impl/HiMessageObserver.java
@@ -1,17 +1,19 @@
 package com.example.pattern.observer.service.impl;
 
 import com.example.pattern.observer.service.api.Observer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.beans.PropertyChangeEvent;
 
 @Service
+@Slf4j
 public class HiMessageObserver implements Observer {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if ("hi".equalsIgnoreCase((String) evt.getNewValue())) {
-            System.out.println("HI message was sent");
+            log.info("*****HI message was sent*****");
         }
     }
 }
